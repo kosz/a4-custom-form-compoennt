7a2b4a2caf17f7a198c1a6a427ac79edc00fb6f1
kosz <coszmin@gmail.com>
2017-06-14 22:47:21 -0400

    :rage::rage::rage: rebelious functional commit; angular pipes should be functions :rage::rage::rage:

--- src/app/myinput/keys.pipe.ts
+++ src/app/myinput/keys.pipe.ts
@@ -1,9 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const transform = (value: any, args?: any[]): any[] =>
+    !!value ? Object.keys(value) : [];
+
 @Pipe({ name: 'keys' })
 export class KeysPipe implements PipeTransform {
-  transform(value: any, args?: any[]): any[] { 
-    return !!value ? Object.keys(value) : []; 
-  }
+  transform = transform;
 }
-
